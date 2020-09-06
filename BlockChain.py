e1629d765a4317e08aac8a34bdad30cd14f67256
owl0109 <kijimakenta19960109@gmail.com>
2020-09-06 11:59:52 +0900

    難易度調整

--- BlockChain.py
+++ BlockChain.py
@@ -52,6 +52,65 @@ class Blockchain():
     def add_newblock(self,i):
         #新しいブロックを作成する
         last_block = self.chain[-1]
-        block = Block(i+1,last_block.block_hash,"ブロック" + str(i+1),datetime.datetime.now(),last_block.bits)
+        #難易度設定をする
+        new_bits = self.get_retarget_bits()
+
+        #難易度設定をしないとき、-1を返す。
+        if new_bits < 0 :
+            bits = last_block.bits
+        else:
+            #難易度設定をする
+            bits = new_bits
+
+        block = Block(i+1,last_block.block_hash,"ブロック" + str(i+1),datetime.datetime.now(),bits)
         self.mining(block)
 
+    def get_retarget_bits(self):
+        #ブロックを5個生成するたびに難易度変更する
+        if len(self.chain) == 0 or len(self.chain) % 5 !=0:
+            return -1
+        #時間の取得
+        expected_time = 140*5
+
+        #最初のブロックを取得
+        if len(self.chain) != 5:
+            first_block = self.chain[-(1+5)]
+        else:
+            first_block = self.chain[0]
+
+        last_block = self.chain[-1]
+
+        #最初のブロック生成の時間を取得
+        first_time = first_block.timestamp.timestamp()
+        #最後のブロック生成時間を取得
+        last_time = last_block.timestamp.timestamp()
+
+        #時間がどの程度かかったか計算する処理
+        total_time = last_time - first_time
+
+        #ターゲットの生成
+        target = last_block.calc_target()
+        #マイニングにかかる時間の比を計算する
+        delta = total_time / expected_time
+
+        if delta < 0.25:
+            delta = 0.25
+        if delta > 4:
+            delta = 4
+
+        new_target = int(target * delta)
+
+        exponent_bytes = (last_block.bits >> 24)-3
+        exponent_bits = exponent_bytes * 8
+        temp_bits = new_target >> exponent_bits
+        #難易度が高すぎるときの処理
+        if temp_bits != temp_bits & 0xffffff:
+            exponent_bytes += 1
+            exponent_bits += 8
+
+        #難易度が低いときの処理
+        elif temp_bits == temp_bits & 0xffff:
+            exponent_bytes -=1
+            exponent_bits -= 8
+        #ビット計算をして返す。
+        return ((exponent_bytes + 3)<<24) | (new_target >> exponent_bits)
